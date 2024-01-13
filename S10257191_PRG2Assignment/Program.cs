db6289088687707748a25d2123558f02e42c1f0b
JosephFed <S10241860@connect.np.edu.sg>
2024-01-13 12:42:51 +0800

    Add Display menu

--- S10257191_PRG2Assignment/Program.cs
+++ S10257191_PRG2Assignment/Program.cs
@@ -1,16 +1,54 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-//Question 1
+//==========================================================
+// Student Number : S10241860
+// Student Name : Joseph Kwek Jun Yan
+// Partner Name : Vallimuthu Rishikesh
+//==========================================================
 
-//Question 2
+void DisplayMenu()
+{
+    Console.WriteLine("Ice Cream Shop Management System");
+    Console.WriteLine("======================================\n" +
+                      "[1] List all customers\n" +
+                      "[2] List all current orders\n" +
+                      "[3] Register a new customer\n" +
+                      "[4] Create a customer’s order\n" +
+                      "[5] Display order details of a customer\n" +
+                      "[6] Modify order details\n" +
+                      "[7] Process an order and checkout\n" +
+                      "[8] Display monthly charged amounts breakdown & total charged amounts for the year\n" +
+                      "[0] Exit");
+}
+while (true)
+{
+    DisplayMenu();
+    Console.Write("Enter your option: ");
+    int opt = Convert.ToInt32(Console.ReadLine());
+    if (opt == 0)
+    {
+        break;
+    }
+    //Question 1
+    else if (opt == 1)
+    {
 
-//Question 3
+    }
+    //Question 2
 
-//Question 4
+    //Question 3
 
-//Question 5
-//Question 6
+    //Question 4
 
-//Advance Question 1
-//Advance Question 2
+    //Question 5
+    //Question 6
+
+    //Advance Question 1
+    //Advance Question 2
+
+}
+
+
+
+
 
